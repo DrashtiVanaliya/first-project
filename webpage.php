115696b732d88d44a3055463e17fd3cfcc81f497
DrashtiVanaliya <drashtivanaliya@gmail.com>
2024-07-03 10:00:28 +0530

    Update webpage.php

--- webpage.php
+++ webpage.php
@@ -1,7 +1,25 @@
-<?php include("conection.php");
-    $email = "";
+<?php 
+$servername = "localhost";
+$username = "root";
+$password ="";
+$dbname ="responsive_form1";
 
+$con = mysqli_connect($servername,$username,$password,$dbname);
+
+
+if($con)
+{
+    echo"connection ok";
+
+}
+else
+{
+    echo"fail".mysqli_connect_error();
+}
 ?>
+    
+
+
 <!DOCTYPE html>
 <html lang="en">
 
